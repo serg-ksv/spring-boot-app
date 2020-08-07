02708f5ec59d4004249419e2c948d9638581d8d9
serg-ksv <serg.ksv1@gmail.com>
2020-08-07 18:40:30 +0300

    fixed query (#11)

--- src/main/java/mate/academy/springbootapp/repository/UserRepository.java
+++ src/main/java/mate/academy/springbootapp/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    @Query(value = "SELECT u.profile_name, u.id FROM user u "
+    @Query(value = "SELECT u.profile_name, u.id FROM users u "
             + "JOIN review r ON u.id = r.user_id "
             + "GROUP BY u.profile_name, u.id "
             + "ORDER BY COUNT(*) DESC LIMIT :limit", nativeQuery = true)
